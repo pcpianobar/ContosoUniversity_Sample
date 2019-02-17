bdd8a648e1d53de1a7ae53b05cb6957e57040987
Kihun Jung <pcpianobar@gmail.com>
2019-02-18 08:22:34 +0900

    학생 인덱스 페이지에 검색 상자 추가

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -8,22 +8,58 @@ using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Model;
 using ContosoUniversity.Models;
 
+
 namespace ContosoUniversity.Pages.Students
 {
     public class IndexModel : PageModel
     {
         private readonly ContosoUniversity.Model.SchoolContext _context;
 
-        public IndexModel(ContosoUniversity.Model.SchoolContext context)
+
+        public IndexModel (ContosoUniversity.Model.SchoolContext context)
         {
             _context = context;
         }
 
-        public IList<Student> Student { get;set; }
 
-        public async Task OnGetAsync()
+        public IList<Student> Student { get; set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+
+        public async Task OnGetAsync (string sortOrder, string searchString)
         {
-            Student = await _context.Student.ToListAsync();
+            NameSort = string.IsNullOrEmpty (sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            CurrentFilter = searchString;
+
+            IQueryable<Student> studentIQ = from s in _context.Student select s;
+
+            if (!string.IsNullOrEmpty (searchString))
+            {
+                studentIQ = studentIQ.Where (s =>
+                    s.LastName.Contains (searchString) || s.FirstMidName.Contains (searchString));
+            }
+            
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentIQ = studentIQ.OrderByDescending (s => s.LastName);
+                    break;
+                case "Date":
+                    studentIQ = studentIQ.OrderBy (s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentIQ = studentIQ.OrderByDescending (s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentIQ = studentIQ.OrderBy (s => s.LastName);
+                    break;
+            }
+
+            Student = await studentIQ.AsNoTracking ().ToListAsync ();
         }
     }
 }
